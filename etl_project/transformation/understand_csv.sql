158c60cfc1d5100e31e83503b8ebb9e593ca7a1d
abdullahdogar09 <abdullah009dogar@gmail.com>
2025-11-20 13:24:31 +0500

    Modifies understand_csv.sql

--- etl_project/transformation/understand_csv.sql
+++ etl_project/transformation/understand_csv.sql
@@ -21,3 +21,19 @@ WHERE table_schema = 'RAW_DATA'
 AND table_name = 'MY_TABLE';
 
 -------------------------------------------------------------------------------------------------------------------------------------------------
+-- List all the column names
+SELECT * 
+FROM information_schema.columns
+WHERE table_schema = 'RAW_DATA'
+AND table_name = 'MY_TABLE';
+
+-------------------------------------------------------------------------------------------------------------------------------------------------
+-- Select top 5 rows in table
+SELECT * FROM my_table ORDER BY ID LIMIT 5; 
+
+-- Select bottom 5 rows in table
+SELECT * FROM my_table ORDER BY ID DESC LIMIT 5;
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+-- Number of null values in description column
+SELECT COUNT(*) - COUNT(DESCRIPTION) AS null_description FROM my_table;
