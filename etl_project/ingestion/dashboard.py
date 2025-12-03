f2305a5e76c3bbe8f53921a6a4cde100388c2ee7
abdullahdogar09 <abdullah009dogar@gmail.com>
2025-12-03 10:19:07 +0500

    Added code in dashboard.py

--- etl_project/ingestion/dashboard.py
+++ etl_project/ingestion/dashboard.py
@@ -9,19 +9,21 @@ st.title("📊 Interactive Dashboard on Companies Information")
 st.write("This dashboard is created on Companies.csv data")
 
 df = session.sql("""
-    SELECT COUNTRY, STATE, ZIP_CODE
+    SELECT COUNTRY, STATE, ZIP_CODE, COMPANY_ID
     FROM MY_TABLE
-    WHERE COUNTRY IS NOT NULL AND STATE IS NOT NULL AND ZIP_CODE IS NOT NULL
-    LIMIT 20
+    WHERE COUNTRY IS NOT NULL 
+        AND STATE IS NOT NULL 
+        AND ZIP_CODE IS NOT NULL
+        AND COMPANY_ID IS NOT NULL
 """).to_pandas()
 
-country_list = df['COUNTRY'].unique().tolist()
+country_list = sorted(df['COUNTRY'].unique().tolist())
 selected_country = st.selectbox("Select COUNTRY", ["ALL"] + country_list)
 
 if selected_country != "ALL":
-    state_list = df[df['COUNTRY'] == selected_country]['STATE'].unique().tolist()
+    state_list = sorted(df[df['COUNTRY'] == selected_country]['STATE'].unique().tolist())
 else:
-    state_list = df['STATE'].unique().tolist()
+    state_list = sorted(df['STATE'].unique().tolist())
 
 selected_state = st.selectbox("Select STATE", ["ALL"] + state_list)
 
@@ -33,23 +35,47 @@ if selected_country != 'ALL':
 if selected_state != 'ALL':
     filtered_data = filtered_data[filtered_data['STATE'] == selected_state]
 
-plot_df = filtered_data.groupby("COUNTRY").agg(ZIP_CODE = ('ZIP_CODE', 'COUNT')).reset_index()
+# For Scatter Plot
+zip_df = filtered_data.groupby("COUNTRY").agg(ZIP_CODE = ('ZIP_CODE', 'count')).reset_index()
 
-fig = px.scatter(
-    plot_df,
+# For Column Chart
+company_df = filtered_data.groupby("COUNTRY").agg(COMPANY_COUNT = ('COMPANY_ID', 'count')).reset_index()
+
+st.header("Scatter plot - Zip codes by Country")
+# Scatter Plot Figure
+fig_scatter = px.scatter(
+    zip_df,
     x = 'COUNTRY',
-    y = 'ZIP_COUNT',
-    size = 'ZIP_COUNT',
+    y = 'ZIP_CODE',
+    size = 'ZIP_CODE',
     hover_name = 'COUNTRY',
     title = 'Total Zip Codes in each Country'
 )
 
-fig.update_layout(
+# Scatter plot update layout
+fig_scatter.update_layout(
     xaxis_title = 'Country',
     yaxis_title = 'Number of Zip Codes',
     template = 'plotly_white'
 )
 
-st.plotly_chart(fig, use_container_width = True)
-st.subheader("Filtered Data Preview")
+st.plotly_chart(fig_scatter, use_container_width = True)
+
+
+# ------------------------------------------------------------------------------------------------------------
+
+st.subheader("Column Chart - Companies in each Country")
+
+fig_column = px.bar(
+    company_df,
+    x = 'COUNTRY',
+    y = 'COMPANY_COUNT',
+    text = 'COMPANY_COUNT',
+    title = 'Total Companies in each country'
+)
+
+st.plotly_chart(fig_column, use_container_width = True)
 st.dataframe(filtered_data)
+
+
+
